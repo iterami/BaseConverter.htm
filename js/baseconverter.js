27f75de8e5f871e77685967f35b7ea4d04d9b10c
Jan Horava <czechinthemail@gmail.com>
2014-05-05 00:49:11 -0700

    improved handling of invalid characters

--- js/baseconverter.js
+++ js/baseconverter.js
@@ -14,36 +14,60 @@ function update_all(base){
     var loop_counter = 34;
     var loop_ids = 0;
 
-    // if the input we are going to update from isn't empty
-    if(document.getElementById(base).value.length > 0){
-        do{
-            loop_ids = loop_counter + 2;
+    do{
+        loop_ids = loop_counter + 2;
 
-            if(loop_counter != base - 2){
-                document.getElementById(loop_ids).value = parseInt(
-                  document.getElementById(base).value,
-                  base
-                ).toString(loop_ids);
+        if(loop_counter != base - 2){
+            document.getElementById(loop_ids).value = parseInt(
+              document.getElementById(base).value,
+              base
+            ).toString(loop_ids);
 
-                if(document.getElementById(loop_ids).value == 'NaN'){
-                    document.getElementById(loop_ids).value = '';
-                }
+            if(document.getElementById(loop_ids).value == 'NaN'){
+                document.getElementById(loop_ids).value = '';
             }
-        }while(loop_counter--);
-
-    // else clear all inputs
-    }else{
-        do{
-            loop_ids = loop_counter + 2;
-            document.getElementById(loop_ids).value = '';
-        }while(loop_counter--);
-    }
+        }
+    }while(loop_counter--);
 }
 
 // fetch saved base36 value
 document.getElementById(36).value = window.location.search.substring(1);
 update_all(36);
 
-document.getElementById('event-delegator').addEventListener('input',function(e){
+/*document.getElementById('event-delegator').addEventListener('input',function(e){
+    var key = window.event ? event : e;
+    key = key.charCode ? key.charCode : key.keyCode;
+
+    console.log(key);
     update_all(e.target.id);
-});
+});*/
+
+window.onkeyup = function(e){
+    if(document.activeElement.tagName == 'INPUT'){
+        var key = window.event ? event : e;
+        key = String.fromCharCode(key.charCode ? key.charCode : key.keyCode);
+
+        // only handle alphanumeric key presses
+        if(key.match(/^[a-z0-9]+$/i)){
+            // limit available characters based on base
+            var available = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'.substring(0, parseInt(document.activeElement.id));
+
+            // check if pressed key is available
+            // if it is, update all bases
+            if(available.indexOf(key) > -1){
+                update_all(document.activeElement.id);
+
+            // else, blank all bases
+            }else{
+                var loop_counter = 34;
+                var loop_ids = 0;
+                do{
+                    loop_ids = loop_counter + 2;
+                    if(loop_ids != document.activeElement.id){
+                        document.getElementById(loop_ids).value = '';
+                    }
+                }while(loop_counter--);
+            }
+        }
+    }
+};
