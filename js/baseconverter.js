9e52e7af510b0c9bf7d2dfbf9478252dd179dffa
Jan Horava <czechinthemail@gmail.com>
2015-11-06 18:00:06 -0800

    replaced iterami URL with URL constructed from location

--- js/baseconverter.js
+++ js/baseconverter.js
@@ -19,7 +19,7 @@ function clear_all(except_id){
 function save(){
     window.prompt(
       'URL to Save/Share:',
-      'http://iterami.com/BaseConverter.htm/?'
+      location.protocol + '//' + location.host + location.pathname + '?'
         + document.getElementById(36).value
     );
 }
