0354012218367f8079128208a24fe62e6e8c5eee
honzi <czechinthemail@gmail.com>
2017-05-29 15:26:49 -0700

    added core_init(), repo_init(), and began global inputs

--- js/baseconverter.js
+++ js/baseconverter.js
@@ -20,6 +20,37 @@ function clear_bases(except_id){
     }while(loop_counter--);
 }
 
+function repo_init(){
+    // Create inputs.
+    var output = '<input id=clear-all type=button value=Clear><input id=save type=button value=Save><br>';
+    for(var i = 2; i < 19; i++){
+        output += i + '<input id=' + i + '><br>';
+    }
+    document.getElementById('left').innerHTML = output;
+    output = '';
+    for(i = 19; i < 37; i++){
+        output += i + '<input id=' + i + '><br>';
+    }
+    document.getElementById('right').innerHTML = output;
+
+    // Fetch saved base36 value.
+    document.getElementById(36).value = window.location.search.substring(1);
+    update_bases(36);
+
+    var id = 0;
+    var loop_counter = 34;
+    do{
+        id = loop_counter + 2;
+
+        document.getElementById(id).oninput = function(e){
+            update_bases(this.id);
+        };
+    }while(loop_counter--);
+
+    document.getElementById('clear-all').onclick = clear_all;
+    document.getElementById('save').onclick = save;
+}
+
 function save(){
     window.prompt(
       'URL:',
@@ -69,34 +100,3 @@ function update_bases(base){
         }
     }while(loop_counter--);
 }
-
-window.onload = function(){
-    // Create inputs.
-    var output = '<input id=clear-all type=button value=Clear><input id=save type=button value=Save><br>';
-    for(var i = 2; i < 19; i++){
-        output += i + '<input id=' + i + '><br>';
-    }
-    document.getElementById('left').innerHTML = output;
-    output = '';
-    for(i = 19; i < 37; i++){
-        output += i + '<input id=' + i + '><br>';
-    }
-    document.getElementById('right').innerHTML = output;
-
-    // Fetch saved base36 value.
-    document.getElementById(36).value = window.location.search.substring(1);
-    update_bases(36);
-
-    var id = 0;
-    var loop_counter = 34;
-    do{
-        id = loop_counter + 2;
-
-        document.getElementById(id).oninput = function(e){
-            update_bases(this.id);
-        };
-    }while(loop_counter--);
-
-    document.getElementById('clear-all').onclick = clear_all;
-    document.getElementById('save').onclick = save;
-};
