fe8d5f71cef1026e02aa026855ccc50573d58f79
honzi <czechinthemail@gmail.com>
2017-10-15 17:49:42 -0700

    moved uncommon functions to data.js

---
+++ js/data.js
@@ -0,0 +1,71 @@
+'use strict';
+
+function clear_all(){
+    if(window.confirm('Clear?')){
+        clear_bases();
+    }
+}
+
+function clear_bases(except_id){
+    except_id = except_id || -1;
+
+    var id = 0;
+    var loop_counter = 34;
+    do{
+        id = loop_counter + 2;
+
+        if(id != except_id){
+            document.getElementById(id).value = '';
+        }
+    }while(loop_counter--);
+}
+
+function save(){
+    window.prompt(
+      'URL:',
+      window.location.protocol
+        + '//' + window.location.host
+        + window.location.pathname
+        + '?' + document.getElementById(36).value
+    );
+}
+
+function update_bases(base){
+    var base_value = document.getElementById(base).value;
+    if(base_value.length <= 0){
+        clear_bases();
+    }
+
+    var id = 0;
+    var loop_counter = 34;
+    do{
+        id = loop_counter + 2;
+        var value = '';
+
+        if(base != id){
+            value = parseInt(
+              base_value,
+              base
+            ).toString(id);
+
+            if(value == 'NaN'){
+                value = '';
+            }
+
+            document.getElementById(id).value = value;
+        }
+
+        // Check if base input has any illegal characters.
+        var maxkey = value.toLowerCase();
+        if(maxkey.length > 0){
+            maxkey = maxkey.split('').sort()[maxkey.length - 1].charCodeAt(0);
+
+            if((loop_counter < 9 && maxkey > 49 + loop_counter)
+              || maxkey > 88 + loop_counter){
+                // If it does, hide impossible results.
+                clear_bases(base);
+                return;
+            }
+        }
+    }while(loop_counter--);
+}
